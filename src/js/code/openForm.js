56318768c0a205f6c2436550e730846d5745f820
Oleksandr <smiyanaleksandr@gmail.com>
2023-04-21 12:47:48 +0300

    Fix: Changes in adaptive layout

--- src/js/code/openForm.js
+++ src/js/code/openForm.js
@@ -5,10 +5,11 @@ export function openForm (){
 	const wrapper = document.querySelector('.wrapper__container')
 	const mainForm = document.querySelector('.wrapper__form')
 	const burger = document.querySelector('.burger__content')
+	const headerNav = document.querySelector('.header__nav')
 	wrapper.classList.add('hide')
 	burger.style.display = 'none'
 	mainForm.style.display = 'block'
-
+	headerNav.style.display = 'none'
 	updatePage()
 	
 }
